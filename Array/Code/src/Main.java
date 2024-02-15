ed5f9046d4b60cf8e9debc198d2e34d7b3ebfbc0
Sayandeep06 <sayandeepdeyimp@gmail.com>
2024-02-16 00:52:59 +0530

    First DSA documentation

---
+++ Array/Code/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Q: Store 5 roll numbers
+        //syntax of array
+        //datatype[] variable = new datatype[size]
+        int[] roll; //declaration of array. roll getting defined in the stack
+        roll = new int[5]; //initialization where the object gets created in the memory(heap)
+
+    }
+}
