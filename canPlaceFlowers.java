ac17f94e899eb2ce66b23e6d76baacae18f34253
Sayandeep06 <sayandeepdeyimp@gmail.com>
2024-06-23 13:55:08 +0530

    reverseVowels in a string added

---
+++ canPlaceFlowers.java
@@ -0,0 +1,13 @@
+public class canPlaceFlowers {
+    public boolean soln(int flowerbed[], int n){
+        if (n==0)
+            return true;
+        for(int i=0; i<flowerbed.length; i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)){
+                n--;
+                flowerbed[i]=1;
+            }if(n==0) return true;
+        }
+        return false;
+    }
+}
---
+++ reverseVowels.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class reverseVowels{
+    public static void main(String args[]){
+        String s = "hello";
+        String a = reverse(s);
+        System.out.println(a);
+    }
+    public static String reverse(String s){
+        char arr[] = s.toCharArray();
+        int start = 0;
+        int end = arr.length-1;
+        String vowels = "aeiouAEIOU";
+        while(start < end ){
+            while(start < end && vowels.indexOf(arr[start])==-1){
+                start ++;
+            }
+            while(start < end && vowels.indexOf(arr[end])==-1){
+                end --;
+            }
+            char temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+        
+            start ++;
+            end --;
+        }
+        String newWord = new String(arr);
+        return newWord;
+    }
+}
